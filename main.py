64729559fd1dc1bf599032214e81306e6b6a12b9
Sheheryar Parvaz <skipper308@hotmail.ca>
2019-11-02 17:18:23 -0400

    Implement shooting bullets

--- main.py
+++ main.py
@@ -18,9 +18,9 @@ class Player:
         self.r = r
 
     def update(self, dt: float):
-        """Update given a time in seconds"""
-        self.x += dt * self.v + .5 * (dt ** 2) * self.a
+        '''Update given a time in seconds'''
         self.v += dt * self.a
+        self.x += dt * self.v
 
     def draw(self, surf):
         w, h = surf.get_size()
@@ -37,20 +37,66 @@ class Player:
         ly = int(h * (1 - lp[1]))
 
         # Draw the circle and the line
-        pg.draw.circle(bg, (255,255,255), (x,y), self.r)
-        pg.draw.line(bg, (255,255,255), (x,y), (lx,ly), 2)
+        pg.draw.circle(surf, (255,255,255), (x,y), self.r)
+        pg.draw.line(surf, (255,255,255), (x,y), (lx,ly), 2)
+
+
+class Bullets:
+    x = np.empty(shape=(0, 2), dtype=np.float32)
+    v = np.empty(shape=(0, 2), dtype=np.float32)
+    a = np.empty(shape=(0, 2), dtype=np.float32)
+
+    def add(self, x, v=[0.0, 0.0], a=[0.0, 0.0]):
+        '''Add a bullet and return its index.'''
+        self.x = np.append(self.x, [x], axis=0)
+        self.v = np.append(self.v, [v], axis=0)
+        self.a = np.append(self.a, [a], axis=0)
+        return len(self.x) - 1
+
+    def rem(self, index):
+        self.x = np.delete(self.x, index, axis=0)
+        self.v = np.delete(self.v, index, axis=0)
+        self.a = np.delete(self.a, index, axis=0)
+
+    def update(self, dt):
+        self.v += dt * self.a
+        self.x += dt * self.v
+
+    def draw(self, bg):
+        for pos in self.x:
+            w, h = bg.get_size()
+            x = int(pos[0] * w)
+            y = int((1-pos[1]) * h)
+            pg.draw.circle(bg, (255,255,255), (x, y), 3)
+
+
+class Game:
+    def __init__(self, dim):
+        pg.init()
+
+        self.prev_t = time.time()
+        self.curr_t = None
+        self.dt = 0
+
+        # Intialize surface
+        self.screen = pg.display.set_mode(dim)
+        self.bg = pg.Surface(self.screen.get_size()).convert()
+
+    def step(self):
+        self.curr_t = time.time()
+        self.dt = self.curr_t - self.prev_t
+        self.prev_t = self.curr_t
+
+    def clear(self):
+        self.bg.fill((0, 0, 0))
+
+    def draw(self):
+        self.screen.blit(self.bg, (0, 0))
+        pg.display.flip()
 
 
 if __name__ == '__main__':
-    # Initialize
-    pg.init()
-    screen = pg.display.set_mode((900, 900))
-
-    # Intialize surface
-    bg = pg.Surface(screen.get_size())
-    bg = bg.convert()
-
-    # Create player
+    g = Game(dim=(900, 900))
     p = Player(
         m=1.0,
         x=(0.5,0.5),
@@ -59,22 +105,32 @@ if __name__ == '__main__':
         angle=math.pi,
         r=10,
     )
-
-    prev_t = time.time()
-    curr_t = None
-    dt = 0
+    b = Bullets()
 
     key_down = {}
     while True:
+        g.step() # Compute dt
+        p.update(g.dt)
+        b.update(g.dt)
+
         # Event loop
         for e in pg.event.get():
-            if e.type == QUIT:
-                exit(0)
-            elif e.type == KEYDOWN:
+            if e.type == QUIT:      exit(0)
+
+            if e.type == KEYDOWN:
+                if e.key == K_SPACE:
+                    b.add(
+                        x=p.x,
+                        v=(p.v + [
+                            .2 * math.cos(p.angle),
+                            .2 * math.sin(p.angle),
+                        ]),
+                        a=[0, 0]
+                    )
+
                 key_down[e.key] = True
             elif e.type == KEYUP:
                 key_down[e.key] = False
-
 
         if key_down.get(pg.K_UP):
             p.v = 0.3 * np.array([
@@ -89,21 +145,12 @@ if __name__ == '__main__':
         else:
             p.v = np.zeros(2)
 
-        if key_down.get(pg.K_LEFT): p.angle += math.pi/250
-        if key_down.get(pg.K_RIGHT): p.angle -= math.pi/250
+        if key_down.get(pg.K_LEFT):  p.angle += math.pi*g.dt
+        if key_down.get(pg.K_RIGHT): p.angle -= math.pi*g.dt
 
-        # Update loop
-        curr_t = time.time()
-        dt = curr_t - prev_t
-        prev_t = curr_t
+        g.clear()
+        p.draw(g.bg)
+        b.draw(g.bg)
 
-        p.update(dt)
-
-
-        # Draw loop
-        bg.fill((0, 0, 0))
-        p.draw(bg)
-
-        screen.blit(bg, (0, 0))
-        pg.display.flip()
-        time.sleep(.00166)
+        g.draw()
+        time.sleep(.0166) # 60FPS
