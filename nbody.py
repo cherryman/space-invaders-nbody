d9a916a70c5d7cdfbf7483dd2e06c2ba2ca2059b
Jonathan Kalinowski <54950948+jekalinowski8@users.noreply.github.com>
2019-11-02 21:25:16 -0400

    particle mesh now functional

--- nbody.py
+++ nbody.py
@@ -1,18 +1,19 @@
 import numpy as np
 import scipy as sp 
-from matplotlib import animation
 from matplotlib import pyplot as plt
-G=5 #gravitationnal constant
+G=1 #gravitationnal constant
 epsilon=0.01 #prevent gravity to explose at short distance -> collision
-nparticles = 2
+nparticles = 10
 dt = 0.1 #Timestep
 buls = np.zeros((nparticles,5)) #array with every bullets (5 component each: mass, x, y, vx, vy)
 for bul in buls: 
     bul[0]=np.random.rand()
     bul[1]=np.random.rand()
     bul[2]=np.random.rand()
-    bul[3]=np.random.rand()*2-1
-    bul[4]=np.random.rand()*2-1
+    bul[3]=.1*(np.random.rand()*2-1)*0
+    bul[4]=.1*(np.random.rand()*2-1)*0
+    
+    
 def gravity(bul1,bul2):
     # gravitational force of bul2 on bul1 
     m1,x1,y1=bul1[0],bul1[1],bul1[2]
@@ -28,51 +29,68 @@ def calForce(buls): #calculate force between 2 particles
                 forces[i]=gravity(buls[i],buls[j]) #here just the gravity
     return forces
 
+
+def calForceMesh(buls):
+    grid = 100
+    density, bx,by= np.histogram2d(buls[:,1],buls[:,2],bins=grid,range=[[0, 1], [0, 1]],weights=buls[:,0])
+    frho = np.fft.fft2(density)
+    ks = np.fft.fftfreq(grid)
+    k2 = np.abs(ks)*np.abs(ks.reshape(-1,1))
+    k2[:,0]=np.Inf
+    k2[0,:]=np.Inf
+    fphi = -G*frho/k2
+    #fphi = np.nan_to_num(fphi) #try other values? 
+    phi = np.real(np.fft.ifft2(fphi))
+    #plt.figure(0)
+    #plt.imshow(np.real(phi))
+    #plt.figure(1)
+    #plt.imshow(density)
+    xgrad, ygrad = np.gradient(phi)
+    forces = np.empty((nparticles,2))
+    for i in range(len(buls)):
+        x=buls[i,1]
+        y=buls[i,2]
+        digx = np.digitize(x,bx)
+        digy = np.digitize(y,by)
+        force = np.array([xgrad[digx,digy],ygrad[digy,digy]])
+        forces[i]=force
+    
+    
+    
+    
+    return forces
+    
+    
+    
+    
+    
+    
+    
 def takeStep(dt,buls):
-    forces = calForce(buls)
+    forces = calForceMesh(buls)
     for i in range(len(buls)):
         ax,ay = forces[i]/buls[i,0] #Newton 2nd law
         buls[i,3] += ax*dt
         buls[i,4] += ay*dt
         buls[i,1] += (buls[i,3]*dt)
         buls[i,2] += (buls[i,4]*dt)
-        buls[i,1] = buls[i,1]%1 #mod1 to fit in the screen
-        buls[i,2] = buls[i,2]%1
+        #buls[i,1] = buls[i,1]%1 #mod1 to fit in the screen
+        #buls[i,2] = buls[i,2]%1
         
     return buls
 
-fig, ax = plt.subplots()
+#fig, ax = plt.subplots()
 T=5
 t=0
-dt=0.001
-x1=np.empty(0)
-x2=np.empty(0)
-y1=np.empty(0)
-y2=np.empty(0)
+dt=0.05
+
 
 while (t<T): #Simulate from 0 to T
-    plt.close()
     buls = takeStep(dt,buls)
-    x1=np.append(x1,buls[0,1])
-    y1=np.append(y1,buls[0,2])
-    x2=np.append(x2,buls[1,1])
-    y2=np.append(y2,buls[1,2])
-    
+    plt.clf()
+    plt.axis([0,1,0,1])
+    for bul in buls:
+        plt.scatter(bul[1],bul[2])
+    plt.pause(1e-3)
     t=t+dt
     
-    fig, ax = plt.subplots()
-xdata, ydata = [], []
-ln, = ax.plot([], [],'ro')
-
-def init():
-    ax.set_xlim(0, 1)
-    ax.set_ylim(0, 1)
-    return ln,
-
-def update(index):
-    ln.set_data((x1[index],x2[index]), (y1[index],y2[index]))
-    
-    return ln,
-
-ani = animation.FuncAnimation(fig, update, frames=range(len(x1)),init_func=init, blit=True)
-plt.show()
